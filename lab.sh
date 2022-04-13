6ecf88908d427eef97ca6eff748a37579aaa83bb
Yash Gupta <yashbg7@gmail.com>
2022-04-13 06:21:28 +0530

    Mention task numbers

--- lab.sh
+++ lab.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# task 1
+
 if [ $# -ne 2 ]
 then
     echo "Usage message"
     exit 1
 fi
+
+# task 2
 
 if [ ! -f "$1" ]
 then
@@ -12,7 +16,11 @@ then
     exit 1
 fi
 
+# task 3
+
 # awk -F , '{print $1, $2, $3, $5, $6, $7, $10, $11}' $1 | tee $2 space_separated_colleges.txt > /dev/null
 awk -F , '{print $1, $2, $3, $5, $6, $7, $10, $11}' $1 > $2
+
+# task 4
 
 awk -F , '$3 == "Bachelor'\''s" {print $1}' $1 >> $2
