c0adc43c8e274722288f2cd1bbf27e44b7308026
Yash Gupta <yashbg7@gmail.com>
2022-04-13 06:16:55 +0530

    Complete task 4

--- lab.sh
+++ lab.sh
@@ -12,4 +12,7 @@ then
     exit 1
 fi
 
+# awk -F , '{print $1, $2, $3, $5, $6, $7, $10, $11}' $1 | tee $2 space_separated_colleges.txt > /dev/null
 awk -F , '{print $1, $2, $3, $5, $6, $7, $10, $11}' $1 > $2
+
+awk -F , '$3 == "Bachelor'\''s" {print $1}' $1 >> $2
