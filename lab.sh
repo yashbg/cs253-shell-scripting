1cbaaef914dea76da3b01c1923dafcb3c01a428f
Yash Gupta <yashbg7@gmail.com>
2022-04-14 23:10:23 +0530

    Attempt task 5 using uniq

--- lab.sh
+++ lab.sh
@@ -27,9 +27,20 @@ awk -F , '$3 == "Bachelor'\''s" {print $1}' $1 >> $2
 
 # task 5
 
-awk -F , 'NR > 1 {print $6}' $1 | sort | uniq > temp.txt
+awk -F , 'NR > 1 {print $6}' $1 | sort | uniq > geographies.txt
+
+echo Geography: Average Admission Rate >> $2
 
 while read geo
 do
-    awk -F , '$6 == x {print $7}' x="$geo" $1 > temp2.txt
-done < temp.txt
+    awk -F , '$6 == x {print $7}' x="$geo" $1 > admission_rates.txt
+    sum=0
+    i=0
+    while read adm_rate
+    do
+        sum=$(echo "$sum + $adm_rate" | bc)
+        ((i++))
+    done < admission_rates.txt
+    avg=$(echo "$sum / $i" | bc)
+    echo $geo: $avg >> $2
+done < geographies.txt
